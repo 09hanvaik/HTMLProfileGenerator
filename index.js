9092b50090d790f99b5096c105785a4542f0d5da
Harry Vaikkath <hvaikkath20@gmail.com>
2024-01-17 07:28:47 +0000

    added questions

--- index.js
+++ index.js
@@ -11,5 +11,36 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Constants for different operations
+const operations = ["Add an engineer", "Add an intern", "Finish building the team"];
 
+// Function to prompt for manager details
+async function promptManager() {
+  const managerData = await inquirer.prompt([
+    {
+      type: "input",
+      message: "Please enter the manager's name:",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Please enter the manager's Id:",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Please enter the manager's email:",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Please enter the manager's office number:",
+      name: "officeN",
+    },
+  ]);
+
+  const manager = new Manager(managerData.name, managerData.id, managerData.email, managerData.officeN);
+  teamMembers.push(manager);
+
+  await promptTeam();
+}
